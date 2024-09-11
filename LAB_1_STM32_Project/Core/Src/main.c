50a7bcb53b48394ecbf6daa4d80bdccb07e166bf
LAPTOPCUATUI\Do Tuan Anh <tuananhdo0202042gmail.com>
2024-09-11 16:06:54 +0700

    init Part 1

--- LAB_1_STM32_Project/Core/Src/main.c
+++ LAB_1_STM32_Project/Core/Src/main.c
@@ -47,6 +47,7 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -83,6 +84,7 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -131,6 +133,53 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Blinky_Pin|LED_YELLOW_Blinky_Pin|LED_RED_Pin|LED_YELLOW_Pin
+                          |LED_GREEN_Pin|LED_RED2_Pin|LED_YELLOW2_Pin|LED_GREEN2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, A_Pin|B_Pin|C_Pin|D2_Pin
+                          |E2_Pin|F2_Pin|G2_Pin|D_Pin
+                          |E_Pin|F_Pin|G_Pin|A2_Pin
+                          |B2_Pin|C2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_RED_Blinky_Pin LED_YELLOW_Blinky_Pin LED_RED_Pin LED_YELLOW_Pin
+                           LED_GREEN_Pin LED_RED2_Pin LED_YELLOW2_Pin LED_GREEN2_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Blinky_Pin|LED_YELLOW_Blinky_Pin|LED_RED_Pin|LED_YELLOW_Pin
+                          |LED_GREEN_Pin|LED_RED2_Pin|LED_YELLOW2_Pin|LED_GREEN2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : A_Pin B_Pin C_Pin D2_Pin
+                           E2_Pin F2_Pin G2_Pin D_Pin
+                           E_Pin F_Pin G_Pin A2_Pin
+                           B2_Pin C2_Pin */
+  GPIO_InitStruct.Pin = A_Pin|B_Pin|C_Pin|D2_Pin
+                          |E2_Pin|F2_Pin|G2_Pin|D_Pin
+                          |E_Pin|F_Pin|G_Pin|A2_Pin
+                          |B2_Pin|C2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
